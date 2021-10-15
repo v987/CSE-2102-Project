e8ef1a99254bd2104a80a72893dafada905f9791
remyjeff <57572996+remyjeff@users.noreply.github.com>
2021-10-15 19:21:47 -0400

    Implemented CustomerProf and CustomerProfDB. Some details are missing tho.

--- IntegratedCustomerSystem/src/CustomerProf.java
+++ IntegratedCustomerSystem/src/CustomerProf.java
@@ -1,5 +1,47 @@
 public class CustomerProf
 {
+    private String adminID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String  phone;
+    private float income;
+    private String status;
+    private String use;
+    private VehicleInfo vehicleInfo;
+
+    public CustomerProf(String adminID, String firstName, String lastName, String address, String phone, float income, String status, String use, VehicleInfo vehicleInfo){
+        this.adminID =  adminID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.income = income;
+        this.status =  status;
+        this.use = use;
+        this.vehicleInfo = vehicleInfo;
+    }
+
+    public String getAdminID(){return adminID;}
+    public float getIncome() {return income;}
+    public String getAddress() {return address;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getPhone() {return phone;}
+    public String getStatus() {return status;}
+    public String getUse() {return use;}
+    public VehicleInfo getVehicleInfo() {return vehicleInfo;}
+
+    public void updateAddress(String address) {this.address = address;}
+    public void updateAdminID(String adminID) {this.adminID = adminID;}
+    public void updateFirstName(String firstName) {this.firstName = firstName;}
+    public void updateLastName(String lastName) {this.lastName = lastName;}
+    public void updateIncome(float income) {this.income = income;}
+    public void updatePhone(String phone) {this.phone = phone;}
+    public void updateStatus(String status) {this.status = status;}
+    public void updateUse(String use) {this.use = use;}
+    public void updateVehicleInfo(VehicleInfo vehicleInfo) {this.vehicleInfo = vehicleInfo;}
+
     public static void main(String[] args){
         System.out.println("Hello, It's Jeff.");
     }
--- IntegratedCustomerSystem/src/CustomerProfDB.java
+++ IntegratedCustomerSystem/src/CustomerProfDB.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+
 public class CustomerProfDB {
+    private int numCustomer;
+    private int currentCustomerIndex;
+    private String fileName;
+    private ArrayList<CustomerProf> customerList;
+
+    public CustomerProfDB(String name){
+        fileName = name;
+    }
+
+    public void insertNewProfile(CustomerProf customer){
+        customerList.add(customer);
+        numCustomer++;
+    }
+    public boolean deleteProfile(String lastName, String adminID){
+        if (customerList.removeIf(customer -> (customer.getAdminID().equals(adminID) && customer.getLastName().equals(lastName)))){
+            numCustomer--;
+            return true;
+        } return false;
+    }
+    public CustomerProf findProfile(String lastName, String adminID){
+        for (CustomerProf customer : customerList){
+            if (customer.getAdminID().equals(adminID) && customer.getLastName().equals(lastName)){
+                return customer;
+            }
+        }
+        return null;
+    }
+    public CustomerProf findFirstProfile(){
+        if (customerList.isEmpty()){return null;}
+        return customerList.get(0);
+    }
+    public CustomerProf findNextProfile(){
+        if (currentCustomerIndex+1 < customerList.size()){
+            return customerList.get(currentCustomerIndex+1);
+        }
+        return null;
+    }
+    public void writeAllCustomerProf(){
+        /**
+         * Need to open file and append all of the customer information into it.
+         */
+        for (CustomerProf customer : customerList){
+            String result = customer.getAdminID()+customer.getFirstName()+customer.getLastName()+
+                    customer.getAddress()+customer.getPhone()+String.valueOf(customer.getIncome())+
+                    customer.getStatus()+customer.getUse();
+
+        }
+    }
+    public static void main(String []args){
+        System.out.println("Hello, Jeff.");
+    }
 }
